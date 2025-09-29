9c9e56af152cc6f2d93e67fc834b659e509fef1d
Sebastián Ortiz <sortiz7841@uta.edu.ec>
2025-09-29 13:02:36 -0500

    fix babel

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -9,6 +9,12 @@ module.exports = {
     moduleNameMapper: {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub'
+    },
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    globals: {
+      'ts-jest': {
+        useESM: true
+      }
     }
   }
   
